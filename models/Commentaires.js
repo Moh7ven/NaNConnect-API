e25511f87ce24d155b39e42e17043a19ccd399d6
Moh7ven <Mohamedsangare0107@gmail.com>
2024-02-13 23:45:33 +0000

    teste

--- models/Commentaires.js
+++ models/Commentaires.js
@@ -1,1 +1,7 @@
 import mongoose from "mongoose";
+
+const commentaireSchema = mongoose.Schema({
+    idNanien: { type: String, required: true },
+    idPub: { type: String, required: true },
+    contenu: { type: String, required: true },
+});
