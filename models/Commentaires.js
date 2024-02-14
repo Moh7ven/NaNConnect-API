5cfcf178fd944bd9b7fd1991984b7ac56c8b1b7b
Moh7ven <Mohamedsangare0107@gmail.com>
2024-02-14 21:13:30 +0000

    Comment model

--- models/Commentaires.js
+++ models/Commentaires.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
 const commentaireSchema = mongoose.Schema({
-    idNanien: { type: String, required: true },
-    idPub: { type: String, required: true },
-    contenu: { type: String, required: true },
+  idNanien: { type: String, required: true },
+  idPub: { type: String, required: true },
+  contenuCom: { type: String, required: true },
 });
+
+export default commentaireSchema = mongoose.model(
+  "Commentaires",
+  commentaireSchema
+);
