b36f520f4ec546c0e6387d4748e5883f5e3158e1
Moh7ven <Mohamedsangare0107@gmail.com>
2024-02-18 23:15:19 +0000

    publications started

--- Controllers/publicationsController.js
+++ Controllers/publicationsController.js
@@ -1,3 +1,12 @@
 import Publications from "../models/Publications";
 
-export const addPublication = (req, res) => {};
+export const addPublication = (req, res) => {
+    const publication = new Publications({
+        idNanien: req.body.idNanien,
+        libPub: req.body.libPub,
+        createdAtPub: req.body.createdAtPub,
+        imagePub: req.body.imagePub,
+        modifPub: req.body.modifPub,
+        authorName: req.body.authorName,
+    });
+};
