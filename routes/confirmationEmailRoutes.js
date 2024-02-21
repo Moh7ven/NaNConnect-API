c3b54f585fd120fbe032ec6c2a8ec8698e2e243d
Moh7ven <Mohamedsangare0107@gmail.com>
2024-02-21 10:12:35 +0000

    links bugs fixed

--- routes/confirmationEmailRoutes.js
+++ routes/confirmationEmailRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { verifyCode } from "../controllers/confirmationEmailController.js";
+import { verifyCode } from "../Controllers/confirmationEmailController.js";
 
 const router = express.Router();
 const upload = multer();
