75aa6a8df4c6d785f6219b7ded22e1439a6eb39c
Moh7ven <Mohamedsangare0107@gmail.com>
2024-02-27 23:22:18 +0000

    like controller started

--- Controllers/likePubController.js
+++ Controllers/likePubController.js
@@ -1,1 +1,40 @@
 import LikePub from "../models/LikePub";
+
+//FONCTION POUR RECUPERER LES LIKES DE LA PUB
+export const getLikes = (req, res) => {
+  LikePub.find({ idPub: req.params.idPub })
+    .then((likes) => {
+      res.status(200).json(likes);
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
+
+//FONCTION POUR AJOUTER UN LIKE
+export const addLike = (req, res) => {
+  const like = new LikePub({
+    idPub: req.params.idPub,
+    idNanien: req.auth.nanienId,
+  });
+  like
+    .save()
+    .then(() => {
+      res.status(200).json({ message: "Like ajouter !" });
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
+
+//FONCTION POUR SUPPRIMER UN LIKE
+export const deleteLike = (req, res) => {
+  LikePub.findOne({ idPub: req.params.idPub, idNanien: req.auth.nanienId })
+    .then((like) => {
+      if (!like) {
+        return res.status(401).json({ message: "Like inexistant !" });
+      }
+      LikePub.deleteOne({ _id: like._id })
+        .then(() => {
+          res.status(200).json({ message: "Like supprimer !" });
+        })
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
