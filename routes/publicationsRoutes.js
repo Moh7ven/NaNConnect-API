d2b8034e56a81258bd95f632dc4834dcdadcedbe
Moh7ven <Mohamedsangare0107@gmail.com>
2024-03-04 02:25:08 +0000

    swagger added in publication route

--- routes/publicationsRoutes.js
+++ routes/publicationsRoutes.js
@@ -51,17 +51,120 @@ const router = express.Router();
 
 router.post("/add-publication", authNaniens, upload, addPublication);
 
+/**
+ * @swagger
+ * /api/publications/get-one-publication/{id}:
+ *   description: Affichage d'une publication.
+ *   get:
+ *     summary: Affichage d'une publication.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: L'id de la publication.
+ *     responses:
+ *       200:
+ *         description: Publication affichée avec success !
+ *       400:
+ *         description: Erreur lors de l'affichage de la publication.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
+
 router.get("/get-one-publication/:id", authNaniens, getOnePublication);
 
+/**
+ * @swagger
+ * /api/publications/user-connected-publications:
+ *   description: Affichage des publications d'un utilisateur connecté.
+ *   get:
+ *     summary: Affichage des publications d'un utilisateur connecté.
+ *     responses:
+ *       200:
+ *         description: Publications affichées avec success !
+ *       400:
+ *         description: Erreur lors de l'affichage des publications.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
 router.get(
   "/user-connected-publications",
   authNaniens,
   getAllNaniensPublications
 );
 
+/**
+ * @swagger
+ * /api/publications/all-publications:
+ *   description: Affichage de toutes les publications.
+ *   get:
+ *     summary: Affichage de toutes les publications.
+ *     parameters:
+ *       - in: query
+ *         name: idNaniens
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: L'id du naniens.
+ *
+ *     responses:
+ *       200:
+ *         description: Publications affichées avec success !
+ *       400:
+ *         description: Erreur lors de l'affichage des publications.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
+
 router.get("/all-publications", getAllPublications);
 
+/**
+ * @swagger
+ * /api/publications/delete-publication/{id}:
+ *   description: Suppression d'une publication.
+ *   delete:
+ *     summary: Suppression d'une publication.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: L'id de la publication.
+ *     responses:
+ *       200:
+ *         description: Publication supprimée avec success !
+ *       400:
+ *         description: Erreur lors de la suppression de la publication.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
+
 router.get("/publication-with-comments/:idPub", getPublicationWithComments);
+
+/**
+ * @swagger
+ * /api/publications/delete-publication/{id}:
+ *   description: Suppression d'une publication.
+ *   delete:
+ *     summary: Suppression d'une publication.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: L'id de la publication.
+ *     responses:
+ *       200:
+ *         description: Publication supprimée avec success !
+ *       400:
+ *         description: Erreur lors de la suppression de la publication.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
 
 router.delete("/delete-publication/:id", authNaniens, deletePublication);
 
