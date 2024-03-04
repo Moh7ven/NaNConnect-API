79ab08993aa8c9363f5a2daac3ab3b3a614a2d06
Moh7ven <Mohamedsangare0107@gmail.com>
2024-03-04 23:38:21 +0000

    likes model updated

--- models/LikePub.js
+++ models/LikePub.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
-let LikePubSchema = mongoose.Schema({});
+let LikePubSchema = mongoose.Schema({
+  idPub: { type: String, required: true },
+  idNanien: { type: String, required: true },
+  createdAt: { type: String, required: true },
+});
 
 const LikePub = mongoose.model("LikePub", LikePubSchema);
 
