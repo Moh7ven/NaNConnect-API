df8f7317e06c8c499259948390a3158dc41b2423
Moh7ven <Mohamedsangare0107@gmail.com>
2024-03-04 02:04:52 +0000

    Route for delete comment add

--- Controllers/commentairesController.js
+++ Controllers/commentairesController.js
@@ -36,3 +36,77 @@ export const getAllCommentaires = (req, res) => {
     .then((commentaires) => res.status(200).json(commentaires))
     .catch((error) => res.status(400).json({ error }));
 };
+
+export const getOneCommentaires = (req, res) => {
+  Commentaires.findOne({ _id: req.params.id })
+    .then((commentaires) => res.status(200).json(commentaires))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+export const deleteCommentaires = (req, res) => {
+  const commentId = req.params.id;
+  const nanienId = req.auth.nanienId; // Identifiant de l'utilisateur authentifié
+
+  // Vérifier si l'utilisateur est l'auteur du commentaire
+  Commentaires.findById(commentId)
+    .then((commentaire) => {
+      if (!commentaire) {
+        return res.status(404).json({ error: "Commentaire non trouvé." });
+      }
+
+      // Si l'utilisateur est l'auteur du commentaire, supprimer le commentaire
+      if (commentaire.idNanien === nanienId) {
+        Commentaires.deleteOne({ _id: commentId })
+          .then(() => {
+            res
+              .status(200)
+              .json({ message: "Commentaire supprimé avec succès !" });
+          })
+          .catch((error) =>
+            res
+              .status(500)
+              .json({ error: "Erreur lors de la suppression du commentaire." })
+          );
+      } else {
+        // Sinon, vérifier si l'utilisateur est l'auteur de la publication associée au commentaire
+        Publications.findOne({ _id: commentaire.idPub, idNanien: nanienId })
+          .then((publication) => {
+            if (!publication) {
+              return res
+                .status(403)
+                .json({
+                  error: "Accès non autorisé à la suppression du commentaire.",
+                });
+            }
+
+            // Si l'utilisateur est l'auteur de la publication, supprimer le commentaire
+            Commentaires.deleteOne({ _id: commentId })
+              .then(() => {
+                res
+                  .status(200)
+                  .json({ message: "Commentaire supprimé avec succès !" });
+              })
+              .catch((error) =>
+                res
+                  .status(500)
+                  .json({
+                    error: "Erreur lors de la suppression du commentaire.",
+                  })
+              );
+          })
+          .catch((error) =>
+            res
+              .status(500)
+              .json({
+                error:
+                  "Erreur lors de la vérification de l'accès à la publication.",
+              })
+          );
+      }
+    })
+    .catch((error) =>
+      res
+        .status(500)
+        .json({ error: "Erreur lors de la recherche du commentaire." })
+    );
+};
--- routes/commentaireRoute.js
+++ routes/commentaireRoute.js
@@ -1,7 +1,9 @@
 import {
   addCommentaires,
+  deleteCommentaires,
   getAllCommentaires,
   getCommentairesFromPublication,
+  getOneCommentaires,
 } from "../controllers/commentairesController.js";
 import express from "express";
 import authNaniens from "../middleware/authNaniens.js";
@@ -19,11 +21,15 @@ router.post(
 );
 
 router.get(
-  "/get-comments-from/:idPub",
+  "/get-comments-from-pub/:idPub",
   authNaniens,
   getCommentairesFromPublication
 );
 
 router.get("/all-comments", authNaniens, getAllCommentaires);
+
+router.get("/get-one-comment/:id", authNaniens, getOneCommentaires);
+
+router.delete("/delete-comment/:id", authNaniens, deleteCommentaires);
 
 export default router;
