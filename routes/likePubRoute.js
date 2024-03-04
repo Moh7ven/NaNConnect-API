c2ab7eb3450af6e2bc0853733d4200b57d89c055
Moh7ven <Mohamedsangare0107@gmail.com>
2024-03-04 23:56:10 +0000

    router for get all likes

--- routes/likePubRoute.js
+++ routes/likePubRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authNaniens from "../middleware/authNaniens.js";
-import { addLike } from "../Controllers/likePubController.js";
+import { addLike, getLikes } from "../Controllers/likePubController.js";
 
 const router = express.Router();
 
 router.post("/add-like-pub/:idPub", authNaniens, addLike);
+
+router.get("/get-all-likes-pub/:idPub", authNaniens, getLikes);
 
 export default router;
