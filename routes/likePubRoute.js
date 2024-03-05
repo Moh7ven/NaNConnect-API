c0ee149accf2479af3c65463c7318bc2ba977639
Moh7ven <Mohamedsangare0107@gmail.com>
2024-03-05 23:15:06 +0000

    swagger added

--- routes/likePubRoute.js
+++ routes/likePubRoute.js
@@ -4,7 +4,51 @@ import { addLike, getLikes } from "../Controllers/likePubController.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/publications/get-all-likes-pub/{idPub}:
+ *   description: Affichage des likes d'une publication.
+ *   get:
+ *     summary: Affichage des likes d'une publication.
+ *     parameters:
+ *       - in: path
+ *         name: idPub
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: L'id de la publication.
+ *     responses:
+ *       200:
+ *         description: Likes affichés avec success !
+ *       400:
+ *         description: Erreur lors de l'affichage des likes.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
+
 router.post("/add-like-pub/:idPub", authNaniens, addLike);
+
+/**
+ * @swagger
+ * /api/publications/get-all-likes-pub/{idPub}:
+ *   description: Affichage des likes d'une publication.
+ *   get:
+ *     summary: Affichage des likes d'une publication.
+ *     parameters:
+ *       - in: path
+ *         name: idPub
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: L'id de la publication.
+ *     responses:
+ *       200:
+ *         description: Likes affichés avec success !
+ *       400:
+ *         description: Erreur lors de l'affichage des likes.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
 
 router.get("/get-all-likes-pub/:idPub", authNaniens, getLikes);
 
