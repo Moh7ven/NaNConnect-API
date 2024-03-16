bc9c274bc05b1ffaf6d661a243cf0f028bdc7952
Moh7ven <Mohamedsangare0107@gmail.com>
2024-03-16 23:48:26 +0000

    swagger added

--- routes/notificationsRoutes.js
+++ routes/notificationsRoutes.js
@@ -7,8 +7,36 @@ import {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/notifications/get-notifications:
+ *   description: Affichage des notifications d'un utilisateur connecté.
+ *   get:
+ *     summary: Affichage des notifications d'un utilisateur connecté.
+ *     responses:
+ *       200:
+ *         description: Notifications affichées avec success !
+ *       404:
+ *         description: Erreur lors de l'affichage des notifications.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
 router.get("/get-notifications", authNaniens, getNotifications);
 
+/**
+ * @swagger
+ * /api/notifications/get-all-notifications:
+ *   description: Affichage de toutes les notifications.
+ *   get:
+ *     summary: Affichage de toutes les notifications.
+ *     responses:
+ *       200:
+ *         description: Notifications affichées avec success !
+ *       404:
+ *         description: Erreur lors de l'affichage des notifications.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
 router.get("/get-all-notifications", authNaniens, getAllNotifications);
 
 export default router;
