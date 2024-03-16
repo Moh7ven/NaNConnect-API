2f8f523a8a481c4bed16379eb726b5a371a85ed5
Moh7ven <Mohamedsangare0107@gmail.com>
2024-03-16 23:46:38 +0000

    route for all notifications in mongoose

--- Controllers/notificationsController.js
+++ Controllers/notificationsController.js
@@ -11,3 +11,12 @@ export const getNotifications = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const getAllNotifications = async (req, res) => {
+  try {
+    const notifications = await Notifications.find();
+    res.status(200).json(notifications);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
--- routes/notificationsRoutes.js
+++ routes/notificationsRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import authNaniens from "../middleware/authNaniens.js";
-import { getNotifications } from "../Controllers/notificationsController.js";
+import {
+  getNotifications,
+  getAllNotifications,
+} from "../Controllers/notificationsController.js";
 
 const router = express.Router();
 
 router.get("/get-notifications", authNaniens, getNotifications);
+
+router.get("/get-all-notifications", authNaniens, getAllNotifications);
 
 export default router;
