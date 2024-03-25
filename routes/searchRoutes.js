5af63ded5be4dbeb3db65b81ff3d56cf4f158b9b
Moh7ven <Mohamedsangare0107@gmail.com>
2024-03-25 23:34:48 +0000

    swagger on search

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -10,10 +10,55 @@ import upload from "../middleware/multer-config.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/search/make-search:
+ *   description: Recherche d'un utilisateur.
+ *   post:
+ *     summary: Recherche d'un utilisateur.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               search:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Requête réussie.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
 router.post("/make-search", authNaniens, upload, makeSearch);
 
+/**
+ * @swagger
+ * /api/search/get-all-search-history:
+ *   description: Recupérer toutes les recherches effectuées.
+ *   get:
+ *     summary: Recupérer toutes les recherches effectuées.
+ *     responses:
+ *       200:
+ *         description: Requête réussie.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
 router.get("/get-all-search-history", authNaniens, upload, getAllSearchHistory);
 
+/**
+ * @swagger
+ * /api/search/get-user-connected-search-history:
+ *   description: Recupérer toutes les recherches effectuées par l'utilisateur connecté.
+ *   get:
+ *     summary: Recupérer toutes les recherches effectuées par l'utilisateur connecté.
+ *     responses:
+ *       200:
+ *         description: Requête réussie.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
 router.get(
   "/get-user-connected-search-history",
   authNaniens,
@@ -21,6 +66,25 @@ router.get(
   getByUserConnetectedSearchHistory
 );
 
+/**
+ * @swagger
+ * /api/search/delete-search-history/{id}:
+ *   description: Supprimer une recherche.
+ *   delete:
+ *     summary: Supprimer une recherche.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: L'id de la recherche.
+ *     responses:
+ *       200:
+ *         description: Requête réussie.
+ *       500:
+ *         description: Erreur interne du serveur.
+ */
 router.delete(
   "/delete-search-history/:id",
   authNaniens,
